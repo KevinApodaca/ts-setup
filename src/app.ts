2e22e58f471d297b356ad21e613b407f9c8ef76e
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-03-24 19:56:11 -0600

    add types to express

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
-import express from 'express';
-import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from 'node:constants';
+import express, { Application, Request, Response, NextFunction } from 'express';
 
-const app = express();
+const app: Application = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hello');
 });
 
